955b361f932b13492e34d2b81ec307769b987bdb
Hoang Nguyen <hoanguit@gmail.com>
2025-03-13 19:52:00 +0700

    refactor: simplify auth security page structure by removing unnecessary redirect

--- src/app/auth/security/page.tsx
+++ src/app/auth/security/page.tsx
@@ -1,11 +1,32 @@
-import { redirect } from "next/navigation";
+import smallScreenImg from "@images/auth/small-screen-security.png";
+import Image from "next/image";
+import { FC } from "react";
+import SecurityVerificationForm from "./verification/_components/form";
 
 export const metadata = {
     title: "Account Security | EZEX",
     description: "Manage your account security settings",
 };
 
-export default function SecurityPage() {
-    // Redirect to the verification page
-    redirect("/auth/security/verification");
-}
+const SecurityPage: FC = () => {
+    return (
+        <div className="flex w-full max-w-[28.75rem] flex-col p-1">
+            <h1 className="text-3xl font-bold">
+                Account Security Verification
+            </h1>
+
+            <Image
+                src={smallScreenImg}
+                alt="security verification"
+                className="mt-4 h-[12.5rem] w-[12.5rem] self-center object-cover lg:hidden"
+                width={200}
+                height={200}
+                priority
+            />
+
+            <SecurityVerificationForm />
+        </div>
+    );
+};
+
+export default SecurityPage;
